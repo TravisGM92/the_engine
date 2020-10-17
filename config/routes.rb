89ae1f396de75af0e8c1b9e7d961f8e38cc3dcde
Travis McKinstry <mckinstrytg@gmail.com>
2020-10-17 13:29:09 -0600

    Change routes to match spec path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find', to: 'search#show'
-        get '/find_all', to: 'search#index'
-        get '/random', to: 'random#show'
         get '/:id/items', to: 'items#index'
       end
       resources :items, only: [:index, :show, :create, :update, :destroy]
